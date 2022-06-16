bbde216f72b03c8eb233bbd5602229b2066650f2
Prg-maker <df3296709@gmail.com>
2022-06-16 20:33:38 -0300

    add theme

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
-import {Text, View} from 'react-native'
+import {Text} from 'react-native'
 
+import {Container} from './styles'
 
 
 export  function Home(){
   return(
-    <View style={{flex:1,justifyContent:'center' , alignItems:'center'}}>
+    <Container >
       <Text>Home</Text>
-    </View>
+    </Container>
   )
 }

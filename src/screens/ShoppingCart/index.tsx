51b0c0260557030b09717151d9e7ac4f32e8880a
Prg-maker <df3296709@gmail.com>
2022-08-15 09:38:00 -0300

    modification shopping cart

--- src/screens/ShoppingCart/index.tsx
+++ src/screens/ShoppingCart/index.tsx
@@ -1,14 +1,73 @@
-import { Button } from '../../components/Button';
-import { Container } from './styles';
+import { useState } from "react";
+import { FlatList, Text, View } from "react-native";
+import { getBottomSpace } from "react-native-iphone-x-helper";
+import { Button } from "../../components/Button";
+import { ProductConfirm, PropsProducts } from "../../components/ProductConfirm";
+import { Container, ContainerTotally, ContentSum, Separete, Sum, SumTotally } from "./styles";
 
 export function ShoppingCart() {
+  const [products, setProducts] = useState<PropsProducts[]>([
+    {
+      id: "2",
+      amount: "9",
+      imageUrl:
+        "https://cdn.w600.comps.canstockphoto.com.br/cima-produtos-beleza-fazer-vetor-clip-arte_csp5949486.jpg",
+      price: "500",
+      title:
+        "Scalable, produtos beleza, cosméticos, gráfico, mudança, cheio, like., cobrança, vetorial, cores, tu",
+    },
+
+    {
+      id: "1",
+      amount: "9",
+      imageUrl:
+        "https://cdn.w600.comps.canstockphoto.com.br/cima-produtos-beleza-fazer-vetor-clip-arte_csp5949486.jpg",
+      price: "500",
+      title:
+        "Scalable, produtos beleza, cosméticos, gráfico, mudança, cheio, like., cobrança, vetorial, cores, tu",
+    },
+   
+  ]);
+
   return (
     <Container>
-    
-
-      <Button
-        title='Desing'
+      <FlatList
+        style={{
+          width: "100%",
+          paddingLeft: 12,
+          marginBottom: "80%",
+          marginTop: getBottomSpace() + 25,
+        }}
+        data={products}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <ProductConfirm
+            amount={item.amount}
+            id={item.id}
+            imageUrl={item.imageUrl}
+            price={item.price}
+            title={item.title}
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 10 }}
       />
+          
+      <ContainerTotally>
+        <Separete/>
+
+        <ContentSum>
+          <Sum>Sum:</Sum>
+          <SumTotally>R$ 500</SumTotally>
+        
+        </ContentSum>
+       
+      </ContainerTotally>
+
+      <View style={{position:'absolute'}}>
+        <Button  title="Desing"/>
+          
+      </View>
     </Container>
   );
 }
--- src/screens/ShoppingCart/styles.ts
+++ src/screens/ShoppingCart/styles.ts
@@ -6,3 +6,37 @@ export const Container = styled.View`
   position: relative;
   align-items: center;
 `;
+
+export const ContainerTotally= styled.View`
+  position: absolute;
+  margin-top: 120%;
+`
+
+export const Separete = styled.View`
+
+  width: 390px;
+  height: 3px;
+  background: #9B9B9B;
+  
+`
+
+export const ContentSum = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top:26px;
+
+
+`
+
+export const Sum = styled.Text`
+  color: #fff;
+  font-weight: bold;
+  font-size: 20px;
+  line-height: 24px;
+`
+export const SumTotally = styled.Text`
+  color: #fff;
+  font-weight: bold;
+  font-size: 20px;
+  line-height: 24px;
+`
